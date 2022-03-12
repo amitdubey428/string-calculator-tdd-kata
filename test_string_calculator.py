c7feef54e457a155a317714de94a5588c1baa1d1
Amit Dubey <dubeyamit.1920@gmail.com>
2022-03-12 20:11:02 +0530

    Adds test case for single number

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -3,17 +3,22 @@ import unittest
 from string_calculator import StringCalculator
 
 class TestStringCalculator(unittest.TestCase):
-
+    
+    global calc
+    calc = StringCalculator() #calc object to be used for all test cases
+    
     def test_empty_addition(self):
-        calc = StringCalculator()
         result = calc.Add("")
         self.assertEqual(0,result)
 
     def test_white_space_addition(self):
-        calc = StringCalculator()
         result = calc.Add(" ")
         self.assertEqual(0,result)
 
+    def test_addition_one_number(self):
+        result = calc.Add("1")
+        self.assertEqual(1,result)
+        
 
 #We don't need this as we will be using nosetests
 # if __name__ == '__main__':
