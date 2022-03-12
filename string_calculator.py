b120c94f6897fe68cfd6f8f38ae16761f4a111dc
Amit Dubey <dubeyamit.1920@gmail.com>
2022-03-13 00:15:42 +0530

    Refactors code to add comma seperated blank string

--- string_calculator.py
+++ string_calculator.py
@@ -2,13 +2,12 @@ class StringCalculator(object):
     def Add(self,numbers):
         try:
             numbers = numbers.strip()
-            if len(numbers) != 0:
-                numbers = numbers.split(",")
-                int_list = list(map(int,numbers))
-                if len(int_list) <=2:
-                    return sum(int_list)
-                return("Oops you can add only two numbers!")
-            return len(numbers)
+            numbers = numbers.split(",")
+            numbers[:] = [x if x != "" else 0 for x in numbers]
+            int_list = list(map(int,numbers))
+            if len(int_list) <=2:
+                return sum(int_list)
+            return("Oops you can add only two numbers!")
         except ValueError:
             return("Oops not a number!")
             
