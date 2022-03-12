cb9189629617537b339dd0d858ab2e853b509808
Amit Dubey <dubeyamit.1920@gmail.com>
2022-03-12 19:59:31 +0530

    Calculator passes first test

---
+++ string_calculator.py
@@ -0,0 +1,4 @@
+class StringCalculator(object):
+    def Add(self,x):    
+        if len(x) == 0:
+            return len(x)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,5 +1,7 @@
 import unittest
 
+from string_calculator import StringCalculator
+
 class TestStringCalculator(unittest.TestCase):
 
     def test_empty_addition(self):
