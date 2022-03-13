5c6bbf344a5695e8cfee47c72e3afe3982c43bc9
Amit Dubey <dubeyamit.1920@gmail.com>
2022-03-13 19:16:51 +0530

    Handles multiple delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -4,20 +4,23 @@ import re
 class StringCalculator(object):
     def Add(self,numbers):
         
-        default_delimiter = ','
+        default_delimiter = ',|'
         long_delimiter_regex = re.compile("^//\[.*\]\\n")  # regex to check for long delimiter
         
         try:
             numbers = numbers.strip()
             if "//" in numbers and numbers.index("//") == 0:  ## Outer if to check custom delimiter  
                 if long_delimiter_regex.match(numbers):  ## To check long delimiter
-                    default_delimiter="".join(map(re.escape, numbers[numbers.index("//")+3:numbers.index("]")]))
-                    numbers=numbers[numbers.index("]")+2:]
+                    default_delimiter=""
+                    delimiter_match = long_delimiter_regex.match(numbers).group()[3:-2]
+                    for x in delimiter_match:
+                        default_delimiter+= (re.escape(x)) + "|"
+                    numbers = numbers[long_delimiter_regex.search(numbers).end():]
                 else:
-                    default_delimiter = str(numbers[2])
+                    default_delimiter = str(numbers[2])+"|"
                     numbers = numbers[numbers.index("//")+3:]
 
-            delimiter_regex = default_delimiter+"|\n"
+            delimiter_regex = default_delimiter+"\n"
             numbers = re.split(delimiter_regex,numbers)
             numbers[:] = [int(x) if x != "" and int(x) <= 1000 else 0 for x in numbers] # Changes value of null strings and x>1000 to 0
             negative_list = list(filter(lambda x:x<0,numbers))
