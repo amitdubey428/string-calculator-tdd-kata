56284873e9dded03610e45f249df0137ffefa626
Amit Dubey <dubeyamit.1920@gmail.com>
2022-03-13 14:02:00 +0530

    Refactors regex logic for hyphen delimiter

--- string_calculator.py
+++ string_calculator.py
@@ -9,8 +9,8 @@ class StringCalculator(object):
             if "//" in numbers and numbers.index("//") == 0:
                 default_delimiter = str(numbers[2])
                 numbers = numbers[3:]
-            delimiter_regex = [default_delimiter+"\n"]
-            numbers = re.split(str(delimiter_regex),numbers)
+            delimiter_regex = default_delimiter+"|\n"
+            numbers = re.split(delimiter_regex,numbers)
             numbers[:] = [x if x != "" else 0 for x in numbers] # Changes value of null strings to 0
             int_list = list(map(int,numbers))
             return sum(int_list)
