9cf295cd6a64ae72a83dbdc65301eee80382ae51
Amit Dubey <dubeyamit.1920@gmail.com>
2022-03-13 11:22:39 +0530

    Refactors code to use regex

--- string_calculator.py
+++ string_calculator.py
@@ -1,8 +1,11 @@
+import re
+
+
 class StringCalculator(object):
     def Add(self,numbers):
         try:
             numbers = numbers.strip()
-            numbers = numbers.split(",")
+            numbers = re.split('[, \n]',numbers)
             numbers[:] = [x if x != "" else 0 for x in numbers]
             int_list = list(map(int,numbers))
             return sum(int_list)    
