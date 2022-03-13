017bb73026ed32ce3052d52a4a07467430879b91
Amit Dubey <dubeyamit.1920@gmail.com>
2022-03-13 18:22:50 +0530

    Adds TC for multiple delimiter

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -84,6 +84,10 @@ class TestStringCalculator(unittest.TestCase):
     def test_addition_long_delimiter(self):
         result = calc.Add("//[***]\n1***2***3")
         self.assertEqual(1+2+3,result)
+
+    def test_addition_multiple_delimiter(self):
+        result = calc.Add("//[*][%]\n1*2%3")
+        self.assertEqual(1+2+3,result)
         
 #We don't need this as we will be using nosetests
 # if __name__ == '__main__':
